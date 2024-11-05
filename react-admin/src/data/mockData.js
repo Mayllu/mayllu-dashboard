63efbd25963f0d9a53e2a3179645eafb35aeefa4
matiascastill0 <matias.castillo@utec.edu.pe>
2024-11-05 18:59:37 -0500

    cbalbuena profile added

--- react-admin/src/data/mockData.js
+++ react-admin/src/data/mockData.js
@@ -3,16 +3,16 @@ import { tokens } from "../theme";
 export const mockDataTeam = [
   {
     id: 1,
-    name: "Jon Snow",
-    email: "jonsnow@gmail.com",
+    name: "Carlos Balbuena",
+    email: "cbalbuena@gmail.com",
     age: 35,
     phone: "(665)121-5454",
     access: "admin",
   },
   {
     id: 2,
-    name: "Cersei Lannister",
-    email: "cerseilannister@gmail.com",
+    name: "Matias Castillo",
+    email: "matiascast@gmail.com",
     age: 42,
     phone: "(421)314-2288",
     access: "manager",
